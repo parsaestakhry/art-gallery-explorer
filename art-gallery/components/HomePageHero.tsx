29a2557fca9d21826ce3db63a15c913cb7a62001
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-10-15 10:34:14 +0330

    implemented stylings on the home page hero

--- art-gallery/components/HomePageHero.tsx
+++ art-gallery/components/HomePageHero.tsx
@@ -5,20 +5,21 @@ export const HomePageHero = () => {
     <div
       className="hero min-h-screen"
       style={{
-        backgroundImage:
-          `url(${image.src})`,
+        backgroundImage: `url(${image.src})`,
       }}
     >
-      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-overlay bg-opacity-70"></div>
       <div className="hero-content text-neutral-content text-center">
         <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-          <p className="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+          <h1 className="mb-5 text-5xl font-bold text-slate-200 ">
+            Welcome to the show!
+          </h1>
+          <p className="mb-5 text-xl font-semibold text-slate-300 ">
+            Visit the unique collection of our artworks powered by the institue
+            of chicago's api.
+            <p className="mt-2 text-lg">Click on the button to learn more !</p>
           </p>
-          <button className="btn btn-primary">Get Started</button>
+          <button className="btn bg-[#caccd5] border-none text-slate-700 ">Learn more</button>
         </div>
       </div>
     </div>
