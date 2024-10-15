51a28cbd491b6476ad24d23682c7681d6d64609c
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-10-15 12:01:19 +0330

    fetched data

--- art-gallery/app/page.tsx
+++ art-gallery/app/page.tsx
@@ -1,11 +1,34 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import image from "@/public/images/marishka-tsiklauri-xIkqIhBPlV4-unsplash.jpg";
+import { useEffect } from "react";
 
 export default function Home() {
+
+  // artwork state
+  
+
+  // fetching the artworks
+  const getArtWorks = async () => {
+    // using the fetch api
+    const response = await fetch("https://api.artic.edu/api/v1/artworks", {
+      method: "GET",
+    });
+
+    // passing it to a data variable
+    const data = await response.json()
+    // logging the response
+    console.log(data);
+  };
+
+  // using useEffect to call the function
+  useEffect(() => {
+    getArtWorks();
+  }, []);
   return (
-    <div className="bg-[#38598b] min-h-screen">
+    <div className="bg-[#113f67] min-h-screen">
+      {/* home page hero  */}
       <div
         className="hero min-h-screen"
         style={{
@@ -21,9 +44,9 @@ export default function Home() {
             <p className="mb-5 text-xl font-semibold text-slate-300 ">
               Visit the unique collection of our artworks powered by the
               institue of chicago's api.
-              <p className="mt-2 text-lg">
+              {/* <h3 className="mt-2 text-lg">
                 Click on the button to learn more !
-              </p>
+              </h3> */}
             </p>
             <button className="btn bg-[#caccd5] border-none text-slate-700 ">
               Learn more
@@ -31,6 +54,8 @@ export default function Home() {
           </div>
         </div>
       </div>
+      {/* carousel */}
+      <div className="carousel carousel-center bg-inherit rounded-box max-w-screen space-x-4 py-10 px-10"></div>
     </div>
   );
 }
