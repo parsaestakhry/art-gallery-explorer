33833231c8e75d37ebec1e3a2de9e7816a752067
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-10-15 09:48:37 +0330

    interchanged main and body

--- art-gallery/app/layout.tsx
+++ art-gallery/app/layout.tsx
@@ -25,10 +25,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body>
+        <main
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          {children}
+        </main>
       </body>
     </html>
   );
