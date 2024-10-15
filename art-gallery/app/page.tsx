0e6c0a19c7d555f35fca1385eb26f089e9df759d
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-10-15 10:18:17 +0330

    added homepage hero to the main page

--- art-gallery/app/page.tsx
+++ art-gallery/app/page.tsx
@@ -1,7 +1,8 @@
+import { HomePageHero } from "@/components/HomePageHero";
 import Image from "next/image";
 
 export default function Home() {
   return <div className="bg-[#38598b] min-h-screen">
-    
+    <HomePageHero/>
   </div>;
 }
