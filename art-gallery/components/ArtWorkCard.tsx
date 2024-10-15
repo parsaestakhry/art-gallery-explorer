2f235c939c443579aa1834d8d6639c46671d3846
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-10-15 15:37:37 +0330

    passed image url as the src of the image

--- art-gallery/components/ArtWorkCard.tsx
+++ art-gallery/components/ArtWorkCard.tsx
@@ -1,29 +1,25 @@
 import { ArtWorkCardType, ArtWorkType } from '@/types/types';
 import React from 'react'
 
-export const ArtWorkCard = ({image_id,artist_display, artist_title, artwork_type_title, title,date_display, iiif_url } : ArtWorkCardType ) => {
+export const ArtWorkCard = ({image_id,artist_display, artist_title, artwork_type_title, title,date_display, iiif_url, image_url } : ArtWorkCardType ) => {
+  //console.log(image_url)
   return (
-    <div className="card  lg:card-side bg-base-100 shadow-xl">
+    <div className="card  lg:card-side bg-base-100 shadow-xl ">
       <figure>
         <img
-          src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp"
+          className='object-cover h-96'
+          src={image_url}
           alt="Album"
         />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{title}</h2>
         <p>{artist_title}</p>
-        <div className='font-mono' >
-          <ul className='flex flex-col space-y-10 '>
-              <li>
-                Date of Display : {date_display}
-              </li>
-              <li>
-                Artist Display : {artist_display}
-              </li>
-              <li>
-                Artwork type title : {artwork_type_title}
-              </li>
+        <div className="font-mono">
+          <ul className="flex flex-col space-y-10 ">
+            <li>Date of Display : {date_display}</li>
+            <li>Artist Display : {artist_display}</li>
+            <li>Artwork type title : {artwork_type_title}</li>
           </ul>
         </div>
         <div className="card-actions justify-end">
