fc1d485a4b80239e1a7f19bce6f8612266b97970
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-10-16 10:02:08 +0330

    implementing artworks page

--- art-gallery/app/artworks/page.tsx
+++ art-gallery/app/artworks/page.tsx
@@ -1,9 +1,9 @@
 export default function Page () {
     return (
-        <>
-            <div>
-                hello
-            </div>
-        </>
-    )
+      <>
+        {/* filter navbar */}
+        <div className="bg-[#38598b] py-3 flex justify-center text-2xl font-serif text-slate-50 min-h-screen ">hello</div>
+        {/* artworks card */}
+      </>
+    );
 }
