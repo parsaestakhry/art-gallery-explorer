2a74e66ab86e8257ffa45fb078c5e388e8d84a98
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-10-18 17:50:06 +0330

    added search button

--- art-gallery/components/SearchBox.tsx
+++ art-gallery/components/SearchBox.tsx
@@ -3,25 +3,19 @@ import React from "react";
 
 export const SearchBox = () => {
   return (
-    <div className="bg-inherit flex ">
-      <label className="input input-bordered flex items-center gap-2 border-none w-full ">
-        <input type="text" className="grow" placeholder="Search" />
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 16 16"
-          fill="currentColor"
-          className="h-4 w-4 opacity-70"
-        >
-          <path
-            fillRule="evenodd"
-            d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-            clipRule="evenodd"
-          />
-        </svg>
+    <div className="flex bg-[#113f67] ">
+      <label className="input input-bordered bg-[#113f67] flex items-center gap-2 border-none w-full ">
+        <input
+          type="text"
+          className="grow bg-[#113f67] placeholder:text-slate-300 "
+          placeholder="Search In ... "
+        />
+        
       </label>
-      <button className="btn bg-red-500" >
-        <MagnifyingGlass/>
+      <button className="btn btn-ghost border-2">
+        <MagnifyingGlass weight="bold" size={20} />
       </button>
+      
     </div>
   );
 };
