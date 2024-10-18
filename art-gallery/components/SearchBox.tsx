82eb0b644ef7dea194cce1b093f92d1bade513c4
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-10-18 22:50:29 +0330

    added artists label checkbox

--- art-gallery/components/SearchBox.tsx
+++ art-gallery/components/SearchBox.tsx
@@ -14,23 +14,46 @@ export const SearchBox = () => {
       </label>
       {/* search button */}
       <button className="btn btn-ghost border-2">
-        <MagnifyingGlass weight="bold" className="mt-2" color="#ffff"  size={20} />
+        <MagnifyingGlass
+          weight="bold"
+          className="mt-2"
+          color="#ffff"
+          size={20}
+        />
       </button>
       {/* filtering dropdown */}
       <div className="dropdown dropdown-end">
-        <div tabIndex={0} role="button" className="btn m-1 text-nowrap btn-ghost text-slate-100">
+        <div
+          tabIndex={0}
+          role="button"
+          className="btn m-1 sm:text-nowrap btn-ghost text-slate-100 text-wrap"
+        >
           Search filters ...
         </div>
         <ul
           tabIndex={0}
-          className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
+          className="dropdown-content menu bg-[#e7eaf6] rounded-box z-[1] w-52 p-2 shadow text-slate-800"
         >
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <a>Item 2</a>
-          </li>
+          <div className="form-control mr-5">
+            <label className="label cursor-pointer">
+              <span className="label-text text-slate-900 mr-5 ">Artworks</span>
+              <input
+                name="checkbox"
+                type="checkbox"
+                className="checkbox "
+              />
+            </label>
+          </div>
+          <div className="form-control mr-5">
+            <label className="label cursor-pointer">
+              <span className="label-text text-slate-900 mr-5 ">Artists</span>
+              <input
+                name="checkbox"
+                type="checkbox"
+                className="checkbox "
+              />
+            </label>
+          </div>
         </ul>
       </div>
     </div>
