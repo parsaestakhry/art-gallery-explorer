7a51d1bc3625c67646fc42b746c5b06554d249ef
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-10-18 22:53:08 +0330

    only one of the checkboxes can be active now

--- art-gallery/components/SearchBox.tsx
+++ art-gallery/components/SearchBox.tsx
@@ -1,15 +1,27 @@
 import { MagnifyingGlass } from "@phosphor-icons/react";
-import React from "react";
+import React, { useState } from "react";
 
 export const SearchBox = () => {
+  const [selectedCheckbox, setSelectedCheckbox] = useState("");
+
+  const handleCheckboxChange = (value: string) => {
+    if (selectedCheckbox === value) {
+      setSelectedCheckbox(""); // Deselect if already selected
+    } else {
+      setSelectedCheckbox(value);
+    }
+  };
+
+  //console.log(selectedCheckbox)
+
   return (
     <div className="flex bg-[#113f67] ">
-      {/* search box  */}
-      <label className="input input-bordered bg-[#113f67] flex items-center gap-2 border-none w-full ">
+      {/* search box */}
+      <label className="input input-bordered bg-[#113f67] flex items-center gap-2 border-none w-full">
         <input
           type="text"
-          className="grow bg-[#113f67] placeholder:text-slate-300 "
-          placeholder="Search In ... "
+          className="grow bg-[#113f67] placeholder:text-slate-300"
+          placeholder="Search In ..."
         />
       </label>
       {/* search button */}
@@ -36,21 +48,25 @@ export const SearchBox = () => {
         >
           <div className="form-control mr-5">
             <label className="label cursor-pointer">
-              <span className="label-text text-slate-900 mr-5 ">Artworks</span>
+              <span className="label-text text-slate-900 mr-5">Artworks</span>
               <input
                 name="checkbox"
                 type="checkbox"
-                className="checkbox "
+                className="checkbox"
+                checked={selectedCheckbox === "artworks"}
+                onChange={() => handleCheckboxChange("artworks")}
               />
             </label>
           </div>
           <div className="form-control mr-5">
             <label className="label cursor-pointer">
-              <span className="label-text text-slate-900 mr-5 ">Artists</span>
+              <span className="label-text text-slate-900 mr-5">Artists</span>
               <input
                 name="checkbox"
                 type="checkbox"
-                className="checkbox "
+                className="checkbox"
+                checked={selectedCheckbox === "artists"}
+                onChange={() => handleCheckboxChange("artists")}
               />
             </label>
           </div>
