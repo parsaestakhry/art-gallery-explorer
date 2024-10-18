6494ad78affd12369d4f049ca222d9df3c702d12
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-10-18 16:27:14 +0330

    reading the parameters

--- art-gallery/app/artworks/[id]/page.tsx
+++ art-gallery/app/artworks/[id]/page.tsx
@@ -1,7 +1,23 @@
-export default function Page () {
-    return (
-        <div>
-            jello
-        </div>
-    )
+"use client";
+
+import { useEffect } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
+
+export default function Page() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const url = `${pathname}`;
+    const art_id = url.replace('/artworks/', '')
+    console.log(art_id);
+    // You can now use the current URL
+    // ...
+  }, [pathname, searchParams]);
+
+  return (
+    <>
+        
+    </>
+  )
 }
