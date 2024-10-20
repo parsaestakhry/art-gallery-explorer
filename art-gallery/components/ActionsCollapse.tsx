cf59ccf9bf8d726bfbeb409dc8d9d5383b4a1e5f
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-10-20 11:38:17 +0330

    styling on the collapse menu

--- art-gallery/components/ActionsCollapse.tsx
+++ art-gallery/components/ActionsCollapse.tsx
@@ -1,11 +1,12 @@
+import { CaretDown } from "@phosphor-icons/react";
 
 export const ActionsCollapse = () => {
   return (
     <div>
-      <div className="collapse bg-base-200 sm:hidden ">
+      <div className="collapse bg-[#38598b] sm:hidden border-none ">
         <input type="checkbox" />
-        <div className="collapse-title text-xl font-medium">
-          Click me to show/hide content
+        <div className="collapse-title text-xl font-sans text-slate-100 flex   items-center ">
+          Actions <CaretDown className="mt-1 ml-2" weight="bold" />
         </div>
         <div className="collapse-content">
           <p>hello</p>
