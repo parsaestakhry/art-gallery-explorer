09bc5922a04780e1bf6ca85a0d141f18d9851823
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-10-20 11:28:34 +0330

    added drawer

--- art-gallery/components/Drawer.tsx
+++ art-gallery/components/Drawer.tsx
@@ -1,6 +1,35 @@
 
 export const Drawer = () => {
   return (
-    <div>Drawer</div>
-  )
+    <div>
+      <div className="drawer drawer-end sm:hidden ">
+        <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content z-50 ">
+          {/* Page content here */}
+          <label
+            htmlFor="my-drawer-4"
+            className="drawer-button btn btn-primary z-50 "
+          >
+            Open drawer
+          </label>
+        </div>
+        <div className="drawer-side z-50  min-h-screen">
+          <label
+            htmlFor="my-drawer-4"
+            aria-label="close sidebar"
+            className="drawer-overlay"
+          ></label>
+          <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+            {/* Sidebar content here */}
+            <li>
+              <a>Sidebar Item 1</a>
+            </li>
+            <li>
+              <a>Sidebar Item 2</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
